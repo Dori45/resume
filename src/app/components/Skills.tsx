112f4e51601537394bbe6929ad6c5abe06d354c9
DoriTing <doriding@jetfi-tech.com>
2026-05-03 00:09:58 +0800

    upCss
    upCss

--- src/app/components/Skills.tsx
+++ src/app/components/Skills.tsx
@@ -12,21 +12,20 @@ export default function Skills() {
     const data = [
       { icon: "/icons/vue.svg",skills: "Vue 3"},
       { icon: "/icons/nuxt.svg",skills: "Nuxt 4"},
-    { icon: "/icons/react.svg",skills: "React"},
-    { icon: "/icons/next.svg",skills: "Next.ts"},
-    { icon: "/icons/ts.svg",skills: "TypeScript"},
-     {icon: "/icons/js.svg",skills: "JavaScript"},
-    { icon: "/icons/pinia.svg",skills: "Pinia"},
-    { icon: "/icons/tailwindcss.svg",skills: "Tailwind css"},
-
-  ];
+      { icon: "/icons/pinia.svg",skills: "Pinia"},
+      { icon: "/icons/react.svg",skills: "React"},
+      { icon: "/icons/next.svg",skills: "Next.ts"},
+      { icon: "/icons/ts.svg",skills: "TypeScript"},
+      {icon: "/icons/js.svg",skills: "JavaScript"},
+      { icon: "/icons/tailwindcss.svg",skills: "Tailwind css"},
+    ];
 
   return (
     <div className="skillsWarp">
       <h2>技能專長</h2>
       <Row className="tags" id="experience">
         {data.map((item,s) => (
-          <Col className="tag" key={s} xs={{span:6}} sm={{span:6}} md={{span:6}} lg={{span:3}}>
+          <Col className="tag" key={s} xs={{span:5}} sm={{span:5}} md={{span:5}} lg={{span:2}}>
             <div style={{fontSize:'36px'}}>
             <img src={item.icon} alt="" />
             </div>
